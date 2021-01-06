524e6c559b1337dddb362eb13377951735783b2a
DimitriNakos <dimi1nakos@gmail.com>
2021-01-06 10:29:36 -0500

    set up heroku

--- server.js
+++ server.js
@@ -16,11 +16,14 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/budget", {
-  useUnifiedTechnology: 
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+  process.env.MONGODB_URI || 'mongodb://localhost/budget',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 // routes
 app.use(require("./routes/api.js"));
